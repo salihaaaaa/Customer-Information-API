928336977dac1febfd439c102684860ba151ec77
Saliha <saliha.work@gmail.com>
2024-02-07 14:01:27 +0800

    Creating endpoint for getting list of customers

---
+++ src/main/java/com/example/Customer/Information/API/customer/CustomerController.java
@@ -0,0 +1,26 @@
+package com.example.Customer.Information.API.customer;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/customer")
+public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> allCustomers = customerService.getAllCustomers();
+
+        return new ResponseEntity<>(allCustomers, HttpStatus.OK);
+    }
+}
