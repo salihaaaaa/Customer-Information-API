58ba86c57ebe0d5a82c2cb9c2b547d988d00b911
Saliha <saliha.work@gmail.com>
2024-02-07 12:42:26 +0800

    Initial commit

---
+++ src/main/java/com/example/Customer/Information/API/CustomerInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Customer.Information.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInformationApiApplication.class, args);
+	}
+
+}
