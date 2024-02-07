4c938f6c111ff625d9c0c296984166fd39e2419f
Saliha <saliha.work@gmail.com>
2024-02-07 13:19:48 +0800

    Add annotation

--- src/main/java/com/example/Customer/Information/API/customer/CustomerRepository.java
+++ src/main/java/com/example/Customer/Information/API/customer/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.example.Customer.Information.API.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
