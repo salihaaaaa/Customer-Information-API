28510dedfd1cc1a0340ad910ba3f4b08b40a840e
Saliha <saliha.work@gmail.com>
2024-02-07 15:11:28 +0800

    Creating method for updating customer

--- src/main/java/com/example/Customer/Information/API/customer/CustomerService.java
+++ src/main/java/com/example/Customer/Information/API/customer/CustomerService.java
@@ -3,10 +3,13 @@ package com.example.Customer.Information.API.customer;
 import com.example.Customer.Information.API.exception.CustomerNotFoundException;
 import com.example.Customer.Information.API.exception.EmailAlreadyExistsException;
 import com.example.Customer.Information.API.exception.IdNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -47,5 +50,35 @@ public class CustomerService {
 
         customerRepository.deleteById(customerId);
 
+    }
+
+    @Transactional
+    public void updateCustomer(Long customerId, String name, String email,
+                                   Gender gender, LocalDate dateOfBirth) {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new IdNotFoundException("customer with id " + customerId + " does not exist")
+                );
+
+        if (name != null && !name.isEmpty() && !Objects.equals(customer.getName(), name)) {
+            customer.setName(name);
+        }
+
+        if (email != null && !email.isEmpty() && !Objects.equals(customer.getEmail(), email)) {
+            Optional<Customer> customerByEmail = customerRepository.findCustomerByEmail(email);
+
+            if(customerByEmail.isPresent()) {
+                throw new EmailAlreadyExistsException("email " + customerByEmail.get().getEmail() + " already exist!");
+            }
+
+            customer.setEmail(email);
+        }
+
+        if (gender != null && !Objects.equals(customer.getGender(), gender)) {
+            customer.setGender(gender);
+        }
+
+        if (dateOfBirth != null && !Objects.equals(customer.getDateOfBirth(), dateOfBirth)) {
+            customer.setDateOfBirth(dateOfBirth);
+        }
     }
 }
