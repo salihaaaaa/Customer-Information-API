823766c424e1c53ef6191a31d9bfd512f00c8a4d
Saliha <saliha.work@gmail.com>
2024-02-07 13:59:58 +0800

    Creating a method to get the list of customer and throw exception if no customer found

--- src/main/java/com/example/Customer/Information/API/customer/CustomerService.java
+++ src/main/java/com/example/Customer/Information/API/customer/CustomerService.java
@@ -1,6 +1,10 @@
 package com.example.Customer.Information.API.customer;
 
+import com.example.Customer.Information.API.exception.CustomerNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
@@ -8,5 +12,15 @@ public class CustomerService {
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        List<Customer> allCustomers = customerRepository.findAll();
+
+        if (allCustomers.isEmpty()) {
+            throw new CustomerNotFoundException("Customer not found!");
+        }
+
+        return allCustomers;
     }
 }
