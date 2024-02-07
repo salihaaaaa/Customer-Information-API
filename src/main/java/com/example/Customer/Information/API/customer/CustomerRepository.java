8db77e3c10efd3ffb9b3e1891c58800e0f21843f
Saliha <saliha.work@gmail.com>
2024-02-07 14:29:07 +0800

    Creating method to find customer by email

--- src/main/java/com/example/Customer/Information/API/customer/CustomerRepository.java
+++ src/main/java/com/example/Customer/Information/API/customer/CustomerRepository.java
@@ -3,6 +3,10 @@ package com.example.Customer.Information.API.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByEmail(String email);
+
 }
